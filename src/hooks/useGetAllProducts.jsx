925008d79952361aa3d30fa97aa161ef007a3ce4
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-09-01 00:48:05 +0500

    loading state added

--- src/hooks/useGetAllProducts.jsx
+++ src/hooks/useGetAllProducts.jsx
@@ -3,15 +3,21 @@ import React, { useEffect, useState } from "react";
 
 const useGetAllProducts = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async () => {
+    setIsLoading(true);
     try {
+      for (var i = 0; i >= 3000000000; i++) {
+        let myVar = i;
+      }
       const res = await axios.get("https://dummyjson.com/products");
       const data = res?.data?.products;
-
       setProducts(data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -19,7 +25,7 @@ const useGetAllProducts = () => {
     fetchData();
   }, []);
 
-  return products;
+  return { products, isLoading };
 };
 
 export default useGetAllProducts;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,16 +3,22 @@ import ProductCard from "../components/ProductCard";
 import useGetAllProducts from "../hooks/useGetAllProducts";
 
 const Products = () => {
-  const products = useGetAllProducts();
+  const { products, isLoading } = useGetAllProducts();
 
   return (
     <div>
       <h1 className="text-4xl font-bold text-blue-600">All Products</h1>
-      <div className="my-10 grid grid-cols-4">
-        {products?.map((product, key) => (
-          <ProductCard product={product} key={key} />
-        ))}
-      </div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : !isLoading && !products.length ? (
+        <div>No Product Found</div>
+      ) : (
+        <div className="my-10 grid grid-cols-4">
+          {products?.map((product, key) => (
+            <ProductCard product={product} key={key} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
